fbd9863d0fef9cea7dd196299d75fc15573f0108
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-03-22 15:35:40 +0700

    ...

--- src/hw4_20001976_PhamBaThang/Bai5.java
+++ src/hw4_20001976_PhamBaThang/Bai5.java
@@ -1,5 +1,6 @@
 package hw4_20001976_PhamBaThang;
 
+import hw4_20001976_PhamBaThang.bai2.LinkedListStack;
 import hw4_20001976_PhamBaThang.bai3.LinkedListQueue;
 
 import java.util.Scanner;
@@ -8,6 +9,7 @@ public class Bai5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         LinkedListQueue<Character> stringQueue = new LinkedListQueue<>();
+        LinkedListStack<Character> stringStack = new LinkedListStack<>();
         System.out.print("Input String: ");
         String inString = input.nextLine().trim().toLowerCase().replaceAll("[^0-9a-zA-Z]+", "");
         int mid = inString.length() % 2 == 0 ? inString.length() / 2 : inString.length() / 2 + 1;
@@ -16,7 +18,10 @@ public class Bai5 {
             stringQueue.enqueue(inString.charAt(i));
         }
         for (int j = inString.length() - 1; j >= mid - 1; j--) {
-            if (inString.charAt(j) != stringQueue.dequeue()) {
+            stringStack.push(inString.charAt(j));
+        }
+        while (!stringStack.isEmpty() && !stringQueue.isEmpty()) {
+            if (stringStack.pop() != stringQueue.dequeue()) {
                 System.out.println("Not Palindrome");
                 return;
             }
