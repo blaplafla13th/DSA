cb9650843ccb44615c83092314bb5a0b805f3327
blaplafla13th <42430845+blaplafla13th@users.noreply.github.com>
2022-04-28 01:08:07 +0700

    ...

--- src/hw7_20001976/tree/AVL.java
+++ src/hw7_20001976/tree/AVL.java
@@ -2,50 +2,71 @@ package hw7_20001976.tree;
 
 public class AVL<T extends Comparable<? super T>> extends BinarySearchTree<T> {
     @Override
-    protected LinkedBinaryTree.Node<T> insert(LinkedBinaryTree.Node<T> node, T data) {
-        return balance(super.insert(node, data));
+    protected Node<T> insert(Node<T> node, T data) {
+        Node<T> newNode = super.insert(node, data);
+        acceptChildren(newNode);
+        newNode = balance(newNode);
+        acceptChildren(newNode);
+        return newNode;
     }
 
     @Override
-    protected LinkedBinaryTree.Node<T> delete(LinkedBinaryTree.Node<T> node, T data) {
-        return balance(super.delete(node, data));
+    protected Node<T> delete(Node<T> node, T data) {
+        Node<T> newNode = super.delete(node, data);
+        acceptChildren(newNode);
+        newNode = balance(newNode);
+        acceptChildren(newNode);
+        return newNode;
     }
 
-    private LinkedBinaryTree.Node<T> rotateRight(LinkedBinaryTree.Node<T> node) {
-        LinkedBinaryTree.Node<T> leftChild = node.getLeft();
-        node.setLeft(leftChild.getRight());
-        leftChild.setRight(node);
+    private void acceptChildren(Node<T> newNode) {
+        if (newNode != null) {
+            if (newNode.right != null)
+                newNode.right.parent = root;
+            if (newNode.left != null)
+                newNode.left.parent = root;
+        }
+    }
+
+    private Node<T> rotateRight(Node<T> node) {
+        Node<T> leftChild = node.left;
+        if (leftChild != null) {
+            node.left = leftChild.right;
+            leftChild.right = node;
+        }
+        acceptChildren(node);
+        acceptChildren(leftChild);
         return leftChild;
     }
 
-    private LinkedBinaryTree.Node<T> rotateLeft(LinkedBinaryTree.Node<T> node) {
-        LinkedBinaryTree.Node<T> rightChild = node.getRight();
-        node.setRight(rightChild.getLeft());
-        rightChild.setLeft(node);
+    private Node<T> rotateLeft(Node<T> node) {
+        Node<T> rightChild = node.right;
+        if (rightChild != null) {
+            node.right = rightChild.left;
+        }
+        acceptChildren(node);
+        acceptChildren(rightChild);
         return rightChild;
     }
 
-    private int balanceFactor(LinkedBinaryTree.Node<T> node) {
-        return tree.height(node.getRight()) - tree.height(node.getLeft());
+    private int balanceFactor(Node<T> node) {
+        return node == null ? 0 : height(node.right) - height(node.left);
     }
 
-    private LinkedBinaryTree.Node<T> balance(LinkedBinaryTree.Node<T> node) {
-        int balanceFactor = balanceFactor(node);
-        if (balanceFactor < -1) {
-            if (balanceFactor(node.getLeft()) <= 0) {
-                node = rotateRight(node);
-            } else {
-                node.setLeft(rotateLeft(node.getLeft()));
-                node = rotateRight(node);
-            }
+    // https://www.quora.com/How-many-cases-are-there-for-rebalancing-AVL-trees/answer/Paolo-Maresca
+    private Node<T> balance(Node<T> node) {
+        int balance = balanceFactor(node);
+        if (balance < -1 && node.right != null && balanceFactor(node.right) < 0)
+            return rotateLeft(node);
+        if (balance > 1 && node.left != null && balanceFactor(node.left) > 0)
+            return rotateRight(node);
+        if (balance < -1 && node.right != null && balanceFactor(node.right) > 0) {
+            node.left = rotateLeft(node.left);
+            return rotateRight(node);
         }
-        if (balanceFactor > 1) {
-            if (balanceFactor(node.getRight()) >= 0) {
-                node = rotateLeft(node);
-            } else {
-                node.setRight(rotateRight(node.getRight()));
-                node = rotateLeft(node);
-            }
+        if (balance > 1 && node.right != null && balanceFactor(node.left) < 0) {
+            node.right = rotateRight(node.right);
+            return rotateLeft(node);
         }
         return node;
     }
